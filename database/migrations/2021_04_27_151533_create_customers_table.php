201b21a9184f1f7d62b59de181e9db4c51532ca1
Wojciech Gawronski <wojciech@gawronsky.com.pl>
2021-04-29 08:42:30 +0200

    db change, migration and CustomerController: show only active users

--- database/migrations/2021_04_27_151533_create_customers_table.php
+++ database/migrations/2021_04_27_151533_create_customers_table.php
@@ -18,6 +18,7 @@ class CreateCustomersTable extends Migration
             $table->string('name');
             $table->string('email');
             $table->string('notes')->nullable();
+            $table->unsignedInteger('active')->default(1);
             $table->timestamps();
         });
     }
