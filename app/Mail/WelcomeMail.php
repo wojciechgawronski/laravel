e7d7d724a4255547010fca8480c081b81a8122cb
Wojciech Gawronski <wojciech@gawronsky.com.pl>
2021-04-29 19:09:28 +0200

    simple email test module example

---
+++ app/Mail/WelcomeMail.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+/**
+ * Class WelcomeMail
+ *
+ * php artisan make:mail WelcomeMail -m emails.welcome
+ * sandbox: https://mailtrap.io/
+ * Email settings: .env file
+ * @package App\Mail
+ */
+class WelcomeMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->markdown('emails.welcome');
+    }
+}
